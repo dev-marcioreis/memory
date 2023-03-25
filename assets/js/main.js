9a0a1a9393b21f032d359b34616dc38b3c16242d
Marcio Reis <dev.marcioreis@gmail.com>
2023-03-25 12:06:21 -0300

    Desenvolvendo CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,170 @@
 const section = document.querySelector('section')
 const playerCount = document.querySelector('span')
-const playerLives = 6
+const playerLives = 0
 
 playerCount.textContent = playerLives
 
 const getData = () => [
     {
-        imgSrc: '../images/apple.webp',
+        imgSrc: "./assets/images/apple.webp",
         name: 'maçâ'
     },
     {
-        imgSrc: '../images/banana.webp',
+        imgSrc: './assets/images/banana.webp',
         name: 'banana'
     },
     {
-        imgSrc: '../images/kiwi.webp',
+        imgSrc: './assets/images/kiwi.webp',
         name: 'kiwi'
     },
     {
-        imgSrc: '../images/mango.webp',
-        name: 'mango'
+        imgSrc: './assets/images/mango.webp',
+        name: 'manga'
     },
     {
-        imgSrc: '../images/orange.webp',
-        name: 'orange'
+        imgSrc: './assets/images/orange.webp',
+        name: 'laranja'
     },
     {
-        imgSrc: '../images/pear.webp',
-        name: 'pear'
+        imgSrc: './assets/images/pear.webp',
+        name: 'pera'
     },
     {
-        imgSrc: '../images/pineapple.webp',
-        name: 'pineapple'
+        imgSrc: './assets/images/pineapple.webp',
+        name: 'abacaxi'
     },
     {
-        imgSrc: '../images/strawberry.webp',
-        name: 'strawberry'
+        imgSrc: './assets/images/strawberry.webp',
+        name: 'morango'
     },
     {
-        imgSrc: '../images/watermelon.webp',
-        name: 'watermelon'
+        imgSrc: './assets/images/watermelon.webp',
+        name: 'melância'
     },
-]
+    {
+        imgSrc: './assets/images/grape.webp',
+        name: 'uva'
+    },
+    {
+        imgSrc: './assets/images/peach.webp',
+        name: 'pessêgo'
+    },
+    {
+        imgSrc: './assets/images/tangerine.webp',
+        name: 'tagerina'
+    },
+    {
+        imgSrc: './assets/images/lemon.webp',
+        name: 'limão'
+    },
+    {
+        imgSrc: './assets/images/pomegranate.webp',
+        name: 'romã'
+    },
+    {
+        imgSrc: './assets/images/fig.webp',
+        name: 'figo'
+    },
+    {
+        imgSrc: './assets/images/plum.webp',
+        name: 'ameixa'
+    },
+    {
+        imgSrc: './assets/images/apple.webp',
+        name: 'maçâ'
+    },
+    {
+        imgSrc: './assets/images/banana.webp',
+        name: 'banana'
+    },
+    {
+        imgSrc: './assets/images/kiwi.webp',
+        name: 'kiwi'
+    },
+    {
+        imgSrc: './assets/images/mango.webp',
+        name: 'manga'
+    },
+    {
+        imgSrc: './assets/images/orange.webp',
+        name: 'laranja'
+    },
+    {
+        imgSrc: './assets/images/pear.webp',
+        name: 'pera'
+    },
+    {
+        imgSrc: './assets/images/pineapple.webp',
+        name: 'abacaxi'
+    },
+    {
+        imgSrc: './assets/images/strawberry.webp',
+        name: 'morango'
+    },
+    {
+        imgSrc: './assets/images/watermelon.webp',
+        name: 'melância'
+    },
+    {
+        imgSrc: './assets/images/grape.webp',
+        name: 'uva'
+    },
+    {
+        imgSrc: './assets/images/peach.webp',
+        name: 'pessêgo'
+    },
+    {
+        imgSrc: './assets/images/tangerine.webp',
+        name: 'tagerina'
+    },
+    {
+        imgSrc: './assets/images/lemon.webp',
+        name: 'limão'
+    },
+    {
+        imgSrc: './assets/images/pomegranate.webp',
+        name: 'romã'
+    },
+    {
+        imgSrc: './assets/images/fig.webp',
+        name: 'figo'
+    },
+    {
+        imgSrc: './assets/images/plum.webp',
+        name: 'ameixa'
+    },
+];
 
-const data = getData()
+const randomize = () => {
+
+    const cardData = getData()
+    cardData.sort(() => Math.random() - 0.5)
+
+    return cardData
+}
+
+const cardGenerator = () => {
+
+    const cardData = randomize()
+
+    cardData.forEach(item => {
+        
+        const card = document.createElement('div')
+        const face = document.createElement('img')
+        const back = document.createElement('div')
+
+        card.classList = 'card'
+        face.classList = 'face'
+        back.classList = 'back'
+
+        face.src = item.imgSrc
+
+        section.appendChild(card)
+        card.appendChild(face)
+        card.appendChild(back)
+
+    })
+
+}
+
+cardGenerator()
